bf68fc8f48c6bd21dac3064db9e7f4a8493c8494
Zharov-Denis <80144137+Zharov-Denis@users.noreply.github.com>
2021-03-06 15:59:19 +1000

    Add files via upload

---
+++ Тест.pas
@@ -0,0 +1,21 @@
+﻿
+uses GraphABC;
+var
+  x,y:integer;
+ 
+procedure KeyDown(Key: integer);
+begin
+   case Key of
+  VK_Left:  drawCircle(x-20,y,100);
+  VK_Right: drawCircle(x+20,y,100);
+  VK_Up:    drawCircle(x,y-20,100);
+  VK_Down:  drawCircle(x,y+20,100);
+  VK_space: Clear;
+   end;
+end;
+ 
+begin
+  x:=300;
+  y:=200;
+  OnKeyDown := KeyDown;
+end.
